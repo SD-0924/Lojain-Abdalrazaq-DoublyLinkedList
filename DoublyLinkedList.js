0d4f8ddc41256e8d8492ceeb9a6ca6d5913c895e
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-10-19 10:01:23 +0300

    adding the getNode, set, and get methods

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -4,10 +4,9 @@ class DoublyLinkedList{
 
     // for creating a new linked list
     constructor(){
-        // head will be the first node of the linked list
-        this.head = null;
-        // tail will be the last node of the linked list
-        this.tail = null;
+        this.head = null; // head will be the first node of the linked list
+        this.tail = null; // tail will be the last node of the linked list
+        this.length = 0; // length will be the total number of nodes in the linked list
     }
 
     // 1. push(val): add a new node to the end of the linked list
@@ -24,6 +23,7 @@ class DoublyLinkedList{
             tempNode.previous = this.tail; // the new node's previous will be the current tail
             this.tail = tempNode; // the new node will be the new tail of the linked list
         }
+        this.length++; // increase the length of the linked list
     }
 
     // 2. pop(): remove the last node of the linked list
@@ -44,6 +44,7 @@ class DoublyLinkedList{
         // now, removing the links of the popped node
         tempNode.previous = null;
         tempNode.next = null;
+        this.length--;
         return tempNode.data;  
     }
 
@@ -65,6 +66,7 @@ class DoublyLinkedList{
         // now, removing the links of the shifted node
         shiftedNode.previous = null;
         shiftedNode.next = null;
+        this.length--;
         return shiftedNode.data;
     }
 
@@ -79,7 +81,53 @@ class DoublyLinkedList{
             tempNode.next = this.head;
             this.head.previous = tempNode;
             this.head = tempNode;
+            this.length++;
         } 
     }
+
+    // getNode function will be used later in the get and set methods
+    getNode(index) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
+    
+        let current;
+        // Check if the index is in the first half of the list
+        if (index <= Math.floor(this.length / 2)) {
+            current = this.head;
+            for (let i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            // Otherwise, traverse from the tail
+            current = this.tail;
+            for (let i = this.length - 1; i > index; i--) {
+                current = current.previous;
+            }
+        }
+        return current;  // Return the node itself
+    }
+
+    // 5. get(index): get the node at the given index
+    get(index){
+        let current = this.getNode(index);
+        if(current !== null){
+            return current.data;
+        }
+        return null
+    }
+
+    // 6. set(index, val): set the value of the node at the given index
+    set(index, val){
+        let current = this.get(index);
+        if(current !== null){
+            current.data = val;
+            return true;
+        }
+        return false;
+    }
+
+
+
 
 }
